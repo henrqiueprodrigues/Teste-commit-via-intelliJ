6d07bade910c21f61a52d3c02c3ce3485e1cce8a
HenriquePR <henriquepaolettirodrigues@gmail.com>
2022-01-17 18:41:15 -0300

    update codigo: nova classe "livro"

--- src/br/com/dio/primeiro_programa_itellij.java
+++ src/br/com/dio/primeiro_programa_itellij.java
@@ -2,14 +2,63 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
-import java.util.Scanner;
+import java.util.Objects;
+
 
 public class primeiro_programa_itellij {
     public static void main(String[] args) {
-        double a,b;
         Gato gato = new Gato("Lani","marrom", 2);
-        Scanner scan= new Scanner(System. in);
+        livro livro1 = new livro("game of throne", 500);
+
         System.out.println(gato);
-        scan.close();
+        System.out.println(livro1);
+
+    }
+}
+
+class livro{
+    private String nome;
+    private Integer paginas;
+
+    public livro(String nome, Integer paginas) {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof livro)) return false;
+        livro livro = (livro) o;
+        return Objects.equals(getNome(), livro.getNome()) && Objects.equals(getPaginas(), livro.getPaginas());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getPaginas());
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", paginas=" + paginas +
+                '}';
     }
 }
